d977d3b6ea662fa89e8d9da734a1b8a16ab00eed
CHETANAPRIYA <119839862+Chetanapriya2@users.noreply.github.com>
2024-01-25 12:03:37 +0530

    import java.util.*; public class King{     public static void main(String[] args){         Scanner sc = new Scanner(System.in);         int x = sc.nextInt();         int y = sc.nextInt();         int z =(x*5)+(y*7);         System.out.print(z);     } }

---
+++ King_Tours_.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class King{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        int z =(x*5)+(y*7);
+        System.out.print(z);
+    }
+}
