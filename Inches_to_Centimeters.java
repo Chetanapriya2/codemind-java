081ef8afffcbea8c986900595065397cbb9cc77c
CHETANAPRIYA <119839862+Chetanapriya2@users.noreply.github.com>
2024-02-01 23:15:14 +0530

    import java.util.*; public class Height{     public static void main(String[] args)     {         Scanner sc = new Scanner(System.in);         int in = sc.nextInt();         double cm = in*2.54;         System.out.printf("%.2f",cm);     } }

---
+++ Inches_to_Centimeters.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Height{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int in = sc.nextInt();
+        double cm = in*2.54;
+        System.out.printf("%.2f",cm);
+    }
+}
