479b184805408c78dd9b66519609934f0007ab17
CHETANAPRIYA <119839862+Chetanapriya2@users.noreply.github.com>
2024-02-02 21:06:23 +0530

    import java.util.*; public class Average{     public static void main(String[] args){         Scanner sc = new Scanner(System.in);         int x = sc.nextInt();         int b = sc.nextInt();         int c = sc.nextInt();         int a = (3*x)-c-b;         System.out.print(a);     } }

---
+++ Average_weight.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Average{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int a = (3*x)-c-b;
+        System.out.print(a);
+    }
+}
