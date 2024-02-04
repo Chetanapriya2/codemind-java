c4b88d944877ec09975d274debcb14e64b53c936
CHETANAPRIYA <119839862+Chetanapriya2@users.noreply.github.com>
2024-02-04 23:10:48 +0530

    import java.util.*; public class Tickets{     public static void main(String[] args){         Scanner sc = new Scanner(System.in);         int x = sc.nextInt();         if((4*x)<=1000){             System.out.print("YES");         }         else if((x==0))         {             System.out.print("NO");         }         else{             System.out.print("NO");         }     } }

---
+++ Tickets.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Tickets{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        if((4*x)<=1000){
+            System.out.print("YES");
+        }
+        else if((x==0))
+        {
+            System.out.print("NO");
+        }
+        else{
+            System.out.print("NO");
+        }
+    }
+}
