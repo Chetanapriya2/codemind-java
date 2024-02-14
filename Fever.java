9db57f3c743a91729d63db2c57cfac30248da058
CHETANAPRIYA <119839862+Chetanapriya2@users.noreply.github.com>
2024-02-14 12:04:21 +0530

    import java.util.*; public class Fever{     public static void main(String[] args)     {         Scanner sc = new Scanner(System.in);         int f = sc.nextInt();         if(f>98)         {             System.out.println("YES");         }         else         {             System.out.println("NO");
            }
        }
    }

---
+++ Fever.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Fever{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int f = sc.nextInt();
+        if(f>98)
+        {
+            System.out.println("YES");
+        }
+        else
+        {
+            System.out.println("NO");
+
+        }
+    }
+}
